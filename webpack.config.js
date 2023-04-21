a4ae76a839117edea4f1856567291bf61ec01a77
Petr Kolesnikov <petrkolesnikov@Petrs-MacBook-Air.local>
2023-04-21 04:04:49 -0500

    html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports  = {
+  mode: 'production',
+  // entry point
   entry: path.resolve(__dirname, 'src', 'index.js'),
+  // output point (where to put the bundle)
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name].[contenthash].js', // [name] is the entry point name [contenthash] is a hash of the content
+    clean: true, // clean the dist folder before each build
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
 }
