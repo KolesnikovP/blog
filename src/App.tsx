3b1f1207e290bd41d54db7036f3a0b684ddb21ac
Petr Kolesnikov <petrkolesnikov@Petrs-MacBook-Air.local>
2023-04-22 22:50:09 -0500

    add global styles and first boilerplate variant of theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss'
 import {MainPageAsync} from "./Pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./Pages/AboutPage/AboutPage.async";
-import {Suspense} from "react";
+import {Suspense, useState} from "react";
+import './styles/index.scss'
+
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark'
+}
 
 export const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
+    }
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>toggle</button>
             <Link to='/'>Main</Link>
             <Link to='/about'>About</Link>
                 <Suspense fallback={<div>Loading...</div>}>
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = createRoot(rootElement);
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App/>
+            <App />
         </BrowserRouter>
     </React.StrictMode>
 );
