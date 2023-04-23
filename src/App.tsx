ec5187e20b0bc1dbdde30855d3e9b6d698f89c57
Petr Kolesnikov <petrkolesnikov@Petrs-MacBook-Air.local>
2023-04-22 21:55:47 -0500

    react router dom and config for routing in webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import {Counter} from "./Counter/Counter";
+import {Link, Route, Routes} from 'react-router-dom';
 import './index.scss'
+import MainPage from "./Pages/MainPage/MainPage";
+import AboutPage from "./Pages/AboutPage/AboutPage";
+import path from "path";
 
 export const App = () => {
     return (
         <div className='app'>
-            <Counter/>
+            <Link to='/'>Main</Link>
+            <Link to='/about'>About</Link>
+            <Routes>
+                <Route path='/' element={<MainPage/>}/>
+                <Route path='/about' element={<AboutPage/>}/>
+            </Routes>
         </div>
     )
 }
