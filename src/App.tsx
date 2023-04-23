136b0709a71093f8090c8f06ef10efeb05c5f959
petrkolesnikov <ptrklsnkv@gmail.com>
2023-04-22 23:44:39 -0500

    classNames.ts as alternative the same library, but it owns))

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import {Link, Route, Routes} from 'react-router-dom';
 import {MainPageAsync} from "./Pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./Pages/AboutPage/AboutPage.async";
-import {Suspense, useContext, useState} from "react";
+import {Suspense} from "react";
 import './styles/index.scss'
-import {Theme, ThemeContext} from "./theme/ThemeContext";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./styles/helpers/classNames/classNames";
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>toggle</button>
             <Link to='/'>Main</Link>
             <Link to='/about'>About</Link>
