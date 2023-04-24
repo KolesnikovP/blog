01212073ae76e42f25a45591ef7cc372446ca91e
petrkolesnikov <ptrklsnkv@gmail.com>
2023-04-23 20:07:25 -0500

    add first shared component: AppLink, first widget Navbar

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,6 +1,7 @@
 type Mods = Record<string, boolean | string>
 
 export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
+    console.log(additional)
     return [
         cls,
         ...Object.entries(mods)
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import {classNames} from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
+import AppLink, {AppLinkTheme} from 'shared/ui/AppLink/AppLink';
 
 interface NavbarProps {
     className?: string;
@@ -9,9 +9,11 @@ interface NavbarProps {
 
 const Navbar = ({className}: NavbarProps) => {
     return (
-        <div className={classNames(cls.navbar)}>
-            <Link to='/'>Main</Link>
-            <Link to='/about'>About</Link>
+        <div className={classNames(cls.Navbar)}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to='/'>Main</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to='/about'>About</AppLink>
+            </div>
         </div>
     );
 };
