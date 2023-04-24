c75aa284ff7bff7ae2bf3b92d37a3d51f71eaf79
petrkolesnikov <ptrklsnkv@gmail.com>
2023-04-23 19:22:40 -0500

    fix mistake in webpack config entry file is not correct

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {classNames} from 'shared/lib/classNames/classNames';
+import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
@@ -8,7 +9,7 @@ interface NavbarProps {
 
 const Navbar = ({className}: NavbarProps) => {
     return (
-        <div className={classNames('navbar')}>
+        <div className={classNames(cls.navbar)}>
             <Link to='/'>Main</Link>
             <Link to='/about'>About</Link>
         </div>
