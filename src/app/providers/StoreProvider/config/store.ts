4268f26199e738b3909b4e900d6674d29e5e7dba
petrkolesnikov <ptrklsnkv@gmail.com>
2023-05-05 19:04:25 -0500

    new page - ProfilePage.tsx, new entity - profile.ts

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,17 +1,20 @@
 import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
-import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
-import { createReducerManager } from './reduceManager';
+import { StateSchema } from './StateSchema';
+import { createReducerManager } from './reducerManager';
 
-export function createReduxStore(initialState?: StateSchema, asyncReducers?: ReducersMapObject<StateSchema>): ReturnType<typeof configureStore> {
-  const rootReducer: ReducersMapObject<StateSchema> = {
+export function createReduxStore(
+  initialState?: StateSchema,
+  asyncReducers?: ReducersMapObject<StateSchema>,
+) {
+  const rootReducers: ReducersMapObject<StateSchema> = {
     ...asyncReducers,
     counter: counterReducer,
     user: userReducer,
   };
 
-  const reducerManager = createReducerManager(rootReducer);
+  const reducerManager = createReducerManager(rootReducers);
 
   const store = configureStore<StateSchema>({
     reducer: reducerManager.reduce,
@@ -24,3 +27,5 @@ export function createReduxStore(initialState?: StateSchema, asyncReducers?: Red
 
   return store;
 }
+
+export type AppDispatch = ReturnType<typeof createReduxStore>['dispatch'];
