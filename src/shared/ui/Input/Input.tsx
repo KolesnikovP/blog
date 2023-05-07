f465a48a8b32fab65b1acab7522566a7063122df
petrkolesnikov <ptrklsnkv@gmail.com>
2023-05-07 14:13:36 -0500

    It doesn't work - why?

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -36,6 +36,8 @@ export const Input = memo((props: InputProps) => {
   const [isFocused, setIsFocused] = useState(false);
   const [caretPosition, setCaretPosition] = useState(0);
 
+  const isCaretVisible = isFocused && !readonly;
+
   useEffect(() => {
     if (autofocus) {
       setIsFocused(true);
@@ -85,10 +87,10 @@ export const Input = memo((props: InputProps) => {
           readOnly={readonly}
           {...otherProps}
         />
-        {isFocused
+        {isCaretVisible
           && (
             <span
-              className={cls.caret}
+              className={classNames(cls.caret)}
               style={{ left: `${caretPosition * 9.6}px` }}
             />
           )}
