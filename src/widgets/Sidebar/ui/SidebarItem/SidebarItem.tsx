3d6d2b2572b1d66892367e795c9103a2e85fea8b
petrkolesnikov <ptrklsnkv@gmail.com>
2023-05-08 16:14:17 -0500

    Private routes for auth users

--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -2,6 +2,8 @@ import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import React, { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useSelector } from 'react-redux';
+import { getUserAuthData } from 'entities/User';
 import { SidebarItemType } from '../../model/items';
 import cls from './SidebarItem.module.scss';
 
@@ -13,6 +15,12 @@ interface SidebarProps {
 export const SidebarItem = memo((props: SidebarProps) => {
   const { t } = useTranslation();
   const { item, collapsed } = props;
+
+  const isAuth = useSelector(getUserAuthData);
+
+  if (item.authOnly && !isAuth) {
+    return null;
+  }
 
   return (
     <AppLink
