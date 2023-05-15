050741cc6680498091b922aa490961facdb21f58
petrkolesnikov <ptrklsnkv@gmail.com>
2023-05-14 22:25:29 -0500

    ArticleDetails entity and page, with test and storybook!!!!

---
+++ src/entities/Article/model/selectors/articleDetails.test.ts
@@ -0,0 +1,50 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getArticleDetailsData, getArticleDetailsError, getArticleDetailsIsLoading } from './articleDetails';
+
+describe('articleDetails.test', () => {
+  const articleData = {
+    id: '1',
+    title: 'title',
+  };
+  test('should return data', () => {
+    const state: DeepPartial<StateSchema> = {
+      articleDetails: {
+        data: articleData,
+      },
+    };
+    expect(getArticleDetailsData(state as StateSchema)).toEqual(articleData);
+  });
+
+  test('should work with empty state', () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getArticleDetailsData(state as StateSchema)).toEqual(undefined);
+  });
+
+  test('getArticleDetailsError should return error', () => {
+    const state: DeepPartial<StateSchema> = {
+      articleDetails: {
+        error: 'error',
+      },
+    };
+    expect(getArticleDetailsError(state as StateSchema)).toEqual('error');
+  });
+
+  test('should work with empty state', () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getArticleDetailsError(state as StateSchema)).toEqual(undefined);
+  });
+
+  test('getArticleDetailsIsLoading should return true', () => {
+    const state: DeepPartial<StateSchema> = {
+      articleDetails: {
+        isLoading: true,
+      },
+    };
+    expect(getArticleDetailsIsLoading(state as StateSchema)).toEqual(true);
+  });
+
+  test('getArticleDetailsIsLoading should work with empty state', () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getArticleDetailsIsLoading(state as StateSchema)).toEqual(undefined);
+  });
+});
