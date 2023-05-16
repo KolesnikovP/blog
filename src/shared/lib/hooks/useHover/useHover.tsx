4517c271795ac26120644c150c7cfc1456752320
petrkolesnikov <ptrklsnkv@gmail.com>
2023-05-16 11:46:18 -0500

    new hook useHover.tsx for hover

---
+++ src/shared/lib/hooks/useHover/useHover.tsx
@@ -0,0 +1,32 @@
+/*
+* для работы с ховер в js есть специальные события (onMouseEnter, onMouseLeave, onMouseMove)
+* эти события отвечают за положение мыши относительно какого то елемента
+* */
+import { useCallback, useMemo, useState } from 'react';
+
+interface UseHoverBind {
+  onMouseEnter: () => void;
+  onMouseLeave: () => void;
+}
+
+type UseHoverResult = [boolean, UseHoverBind]
+
+export const useHover = (): UseHoverResult => {
+  const [isHover, setIsHover] = useState(false);
+
+  const onMouseEnter = useCallback(() => {
+    setIsHover(true);
+  }, []);
+
+  const onMouseLeave = useCallback(() => {
+    setIsHover(false);
+  }, []);
+
+  return useMemo(() => [
+    isHover,
+    {
+      onMouseEnter,
+      onMouseLeave,
+    },
+  ], [isHover, onMouseEnter, onMouseLeave]);
+};
