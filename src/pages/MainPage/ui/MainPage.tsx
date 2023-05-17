68b69758dca566f6518948a489250619ce94d66d
petrkolesnikov <ptrklsnkv@gmail.com>
2023-05-17 11:05:18 -0500

    Refactor for styles of page-wrapper. Add new component Page with page wrapper styles and wrapped our pages

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,16 +2,17 @@ import React, { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BugButton } from 'app/providers/ErrorBoundary';
 import { Counter } from 'entities/Counter';
+import { Page } from 'shared/ui/Page/Page';
 
 const MainPage = memo(() => {
   const { t } = useTranslation('main');
 
   return (
-    <div>
+    <Page>
       {t('главная страница')}
       <BugButton />
       <Counter />
-    </div>
+    </Page>
   );
 });
 
