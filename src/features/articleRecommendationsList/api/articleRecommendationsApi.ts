cc6ce63e81b4dde1d3f17ffea03f9c8c4b70a419
petrkolesnikov <ptrklsnkv@gmail.com>
2023-07-10 14:28:09 -0500

    added missed typezation in query

--- src/features/articleRecommendationsList/api/articleRecommendationsApi.ts
+++ src/features/articleRecommendationsList/api/articleRecommendationsApi.ts
@@ -1,9 +1,10 @@
 import { rtkApi } from 'shared/api/rtkApi';
+import { Article } from 'entities/Article';
 
 const recommendationsApi = rtkApi.injectEndpoints({
   endpoints: (build) => ({
     // Для запросов используем query, для изменения и создания данных mutation
-    getArticleRecommendationsList: build.query({
+    getArticleRecommendationsList: build.query<Article[], number>({
       query: (limit) => ({
         url: '/articles',
         params: {
