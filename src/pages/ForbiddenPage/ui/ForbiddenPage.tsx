ad80e5e0230911c37c544147620f83d67b82b77e
petrkolesnikov <ptrklsnkv@gmail.com>
2023-07-25 20:15:26 -0500

    add private route with required roles

---
+++ src/pages/ForbiddenPage/ui/ForbiddenPage.tsx
@@ -0,0 +1,14 @@
+import React, { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Page } from 'widgets/Page/Page';
+
+const ForbiddenPage = memo(() => {
+  const { t } = useTranslation('about');
+  return (
+    <Page>
+      {t('Недостаточно прав для доступа к этой странице')}
+    </Page>
+  );
+});
+
+export default ForbiddenPage;
