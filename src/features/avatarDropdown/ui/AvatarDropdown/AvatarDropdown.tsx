487f07ed8920d3b26fdec106ec033a48b854dc4d
petrkolesnikov <ptrklsnkv@gmail.com>
2023-09-12 20:59:45 -0500

    decomposition of navbar

---
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
@@ -0,0 +1,55 @@
+import React, { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Dropdown } from 'shared/ui/Popups';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getUserAuthData, isUserAdmin, isUserManager, userActions,
+} from 'entities/User';
+
+interface AvatarDropdownProps {
+  className?: string;
+}
+
+export const AvatarDropdown = memo((props: AvatarDropdownProps) => {
+  const { className } = props;
+  const { t } = useTranslation();
+
+  const dispatch = useDispatch();
+  const userAuthData = useSelector(getUserAuthData);
+  const isAdmin = useSelector(isUserAdmin);
+  const isManager = useSelector(isUserManager);
+  const onLogout = useCallback(() => {
+    dispatch(userActions.logout());
+  }, [dispatch]);
+
+  const isAdminPanelAvailable = isAdmin || isManager;
+
+  if (!userAuthData) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Dropdown
+        direction='bottom left'
+        items={[
+          ...(isAdminPanelAvailable ? [{
+            content: t('Админка'),
+            href: RoutePath.admin_panel,
+          }] : []),
+          {
+            content: t('Профиль'),
+            href: RoutePath.profile + userAuthData.id,
+          },
+          {
+            content: t('Выйти'),
+            onClick: onLogout,
+          },
+        ]}
+        trigger={<Avatar size={30} src={userAuthData.avatar} />}
+      />
+    </div>
+  );
+});
