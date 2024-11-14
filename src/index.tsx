b3230ff8fed06a6c6c9c35a3f4585c338aa15a00
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2024-11-14 07:26:54 -0600

    minnor for test git hub pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@/app/providers/ThemeProvider';
 import { StoreProvider } from '@/app/providers/StoreProvider';
-import App from './app/App';
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
 import '@/app/styles/index.scss';
-import './shared/config/i18n/i18n';
+import App from './app/App';
 import { ErrorBoundary } from './app/providers/ErrorBoundary';
+import './shared/config/i18n/i18n';
 
 const container = document.getElementById('root');
 
 if (!container) {
     throw new Error('Контейнер root не найден. НЕ удалось вмонтировать реакт приложение');
 }
-
 const root = createRoot(container);
 
 root.render(
