d0100360d222188c7c148f804c0e7a0df9630e22
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2024-12-27 21:46:54 -0600

    first tests cypress

--- cypress/e2e/common/routing.cy.ts
+++ cypress/e2e/common/routing.cy.ts
@@ -1,7 +1,21 @@
 describe('routing', () => {
-    it('go to the main page', () => {
-        // we added base url
-        cy.visit('/');
-        cy.get('[data-testid=MainPage]').should('exist');
+    describe('User NOT authorized', () => {
+        it('go to the main page', () => {
+            // we added base url
+            cy.visit('/');
+            cy.get('[data-testid=MainPage]').should('exist');
+        });
+        it('should get redirected when unauthorized user opens a profile page', () => {
+            cy.visit('/profile/1');
+            cy.get('[data-testid=MainPage]').should('exist');
+        });
+        it('not existing route', () => {
+            cy.visit('/profiledfasfsa');
+            cy.get('[data-testid=NotFoundPage]').should('exist');
+        });
+    });
+
+    describe('User authorized', () => {
+
     });
 });
