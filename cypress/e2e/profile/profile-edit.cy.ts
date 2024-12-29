eb26242c3085f79f0231edcd0642805ac2cd8f04
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2024-12-28 23:10:52 -0600

    e2e 114 article details, article list, profile

--- cypress/e2e/profile/profile-edit.cy.ts
+++ cypress/e2e/profile/profile-edit.cy.ts
@@ -1,5 +1,26 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { selectByTestId } from '../../helpers/selectByTestId';
+
+let profileId = '';
+
+describe('the user has visited the profile page', () => {
+    beforeEach(() => {
+        cy.visit('');
+        cy.login().then((data) => {
+            profileId = data.id;
+            cy.visit(`profile/${data.id}`);
+        });
+    });
+    afterEach(() => {
+        cy.resetProfile(profileId);
+    });
+    it('User\'s profile successfully downloaded', () => {
+        cy.get(selectByTestId('ProfileCard.firstname')).should('have.value', 'test');
+    });
+    it('User edits its profile', () => {
+        const newName = 'new';
+        const newLastname = 'lastname';
+        cy.updateProfile(newName, newLastname);
+        cy.getByTestId('ProfileCard.firstname').should('have.value', newName);
+        cy.getByTestId('ProfileCard.lastname').should('have.value', newLastname);
+    });
+});
