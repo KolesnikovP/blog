eca62fb803d92b5c895887f9a5b89f9a5cf37562
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-11-08 20:28:54 -0600

    Remove unnecessary empty line in router.ts

--- src/shared/const/router.ts
+++ src/shared/const/router.ts
@@ -12,7 +12,6 @@ export enum AppRoutes {
     NOT_FOUND = 'not_found',
 }
 
-
 export const getRouteMain = () => '/';
 export const getRouteAbout = () => '/about';
 export const getRouteProfile = (id: string) => `/profile/${id}`;
